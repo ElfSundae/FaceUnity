a5504a4c1670d8970b541274c78b7b19f0398ff1
Elf Sundae <elf.sundae@gmail.com>
2020-05-07 17:51:55 +0800

    Preload Faceunity dataSource and items

--- Example/AppDelegate.m
+++ Example/AppDelegate.m
@@ -21,6 +21,11 @@
 
     FUSetAuthData(&g_auth_package, sizeof(g_auth_package));
 
+    // Preload Faceunity dataSource and items.
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [FUManager shareManager];
+    });
+
     [self.window makeKeyAndVisible];
     return YES;
 }
